c63139676805daaf1d4ed65eb3eaf01e6a97f717
Elise_LaBonte <emlabonte714@gmail.com>
2021-08-17 11:25:00 -0400

    added .gitignore, utils folder, updated questions

--- index.js
+++ index.js
@@ -1,96 +1,40 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 // const axios = require("axios");
-// const markdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 const questionsForUser =   [
     
-        {   name:  'title',
+        {   type: 'input',
+            name:  'title',
             message: 'what is your project\'s title?'
         },
-        {   name: 'description',
+        {   type: 'input',
+            name: 'description',
             message: 'enter a brief project description'
         },
-        {   name: 'installation',
+        {   type: 'input',
+            name: 'installation',
             message:'enter a project installation instructions'
         },
-        {   name:'usage',
+        {   type: 'input',
+            name:'usage',
             message: 'describe the intended usage of this project'
         },
-        {   name:'license',
+        {   type: 'list',
+            choices: ['MIT', 'GPLv2', 'Apache', 'none'],
+            name:'license',
             message: 'enter your project license (if none, hit enter)'
         },
-        {   name:'contributing',
+        {   type: 'input',
+            name:'contributing',
             message: 'who are the project contributors?'
         },
-        {   name:'tests',
+        {   type: 'input',
+            name:'tests',
             message: 'enter test instructions'
         }
-]
-
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-    if (!license)   {
-        return '';
-    }
-
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-    if (!license)   {
-        return '';
-    }
-
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-    if (!license)   {
-        return '';
-    }
-
-}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-    return `
-    # ${data.title}
-
-    # Table of Content
-    -[description](#description)
-    -[installation](#installation)
-    -[usage](#usage)
-    -[license](#license)
-    -[contributing](#contributing)
-    -[tests](#tests)
-
-    ##username:
-        ${data.username}
-
-    ##description:
-        ${data.description}
-
-    ##installation:
-        ${data.installation}
-
-    ##usage:
-        ${data.usage}
-
-    ##license:
-        ${data.license}
-
-    ##contributing:
-        ${data.contributing}
-
-    ##tests:
-        ${data.tests}
-
-    `;
-};
+];
 
 function init() {
     inquirer.prompt(questionsForUser)
